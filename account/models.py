244522716ac5bec17b6008630455bb6471621f3b
maxim <st119024@student.spbu.ru>
2024-07-07 18:51:55 +0500

    add account/ account/id realisation, add templates

--- account/models.py
+++ account/models.py
@@ -6,3 +6,9 @@ class Account(models.Model):
     id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.id)
+    
+    def id_less_than_ten(self):
+        return True if self.id < 10 else False
--- account/views.py
+++ account/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.views import generic
+
+from .models import Account
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = "account/index.html"
+    model = Account
+    context_object_name = "account_list"
+
+    def get_queryset(self):
+        return Account.objects.order_by("-id")[:10]
+    
+
+class DetailView(generic.DetailView):
+    template_name = "account/detail.html"
+    model = Account
+
